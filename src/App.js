ce3955a316865223c5f7d4228f1e4dd5a27e930a
mso872 <mso872@gmail.com>
2024-05-02 14:39:26 -0700

    Remove React boilerplate, bring components to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import QuestionForm from './components/QuestionForm';
+import Navbar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <main
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "32px",
+        }}
+        className="main-content"
+      >
+        <QuestionForm />
+      </main>
     </div>
   );
 }
