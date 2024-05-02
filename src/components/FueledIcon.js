00a24c27aa3b1b82174c9587f2344a5dd8223368
mso872 <mso872@gmail.com>
2024-05-02 14:38:56 -0700

    Create Icon for nav bar

---
+++ src/components/FueledIcon.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default function FueledIcon() {
+  // const FueledIconStyles = {
+  //     background: url('your-image-url.jpg'),
+  //     height: 20,
+  //     width: 20,
+  //     display: "block",
+  // }
+
+  return (
+    <span>
+      <svg
+        width="48"
+        height="48"
+        viewBox="0 0 48 48"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M0 48H48V0H0V48Z"
+          fill="#AE0000"
+        />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M29.033 13.7517L29.032 22.174H28.0508H19.9184H18.9422L18.9413 13.7589L19.9159 12.7854H28.0689L29.033 13.7517ZM36.6531 32.4593V23.6331C36.6531 23.6333 37.095 23.1916 37.095 23.1916V19.3103L34.7782 16.9758H32.4439V17.6667H34.5035L35.831 19.0155L35.1834 19.6608L35.1822 23.1892L35.6148 23.6183V32.0219H33.6725V22.8385L33.0212 22.1808H30.6549V13.0834L28.746 11.1749H19.2356L17.3265 13.0834V35.0098H15.72V36.623H32.257V35.0098H30.6549V23.235H32.6048V32.4208L33.2564 33.0783H36.0391L36.6531 32.4593Z"
+          fill="#F5F5F1"
+        />
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M36.2042 17.1169V17.6605H36.1061V17.1169H35.873V17.0206H36.4449V17.1169H36.2042ZM36.6009 17.2565V17.6606H36.5028V17.0207H36.5955L36.799 17.4626L37.0024 17.0207H37.0951V17.6606H36.997V17.2574L36.844 17.5922H36.7548L36.6009 17.2565Z"
+          fill="#F5F5F1"
+        />
+      </svg>
+    </span>
+  );
+}
