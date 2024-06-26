90ee753d3b594706bbcf21c2b6933d2fdf52ae8a
mso872 <mso872@gmail.com>
2024-06-25 21:49:55 -0700

    Create footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,68 @@
+import React from "react";
+// date subcomponent
+import PersonalCopyright from "./PersonalCopyright";
+
+const Footer = () => {
+  const footerStyle = {
+    position: "relative",
+    bottom: 0,
+    width: "100%",
+    backgroundColor: "transparent",
+    margin: "2rem",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: "1rem",
+    borderTop: "1px solid #ccc", // Example border styling
+  };
+
+  const linkStyle = {
+    marginLeft: "1rem", // Adjust spacing between links
+    textDecoration: "none",
+    color: "black",
+  };
+
+  const handleLinkHover = (event) => {
+    event.target.style.color = "#666"; // Change color on hover
+    event.target.style.borderBottomColor = "#666"; // Show underline on hover
+  };
+
+  const handleLinkLeave = (event) => {
+    event.target.style.color = "#333"; // Restore default color
+    event.target.style.borderBottomColor = "transparent"; // Hide underline
+  };
+
+  return (
+    <footer style={footerStyle}>
+      <div>
+        <PersonalCopyright />
+        <a
+          href="mailto:mso872@gmail.com"
+          style={linkStyle}
+          onMouseEnter={handleLinkHover}
+          onMouseLeave={handleLinkLeave}
+        >
+          Email
+        </a>
+        <a
+          href="https://github.com/markohanesian"
+          style={linkStyle}
+          onMouseEnter={handleLinkHover}
+          onMouseLeave={handleLinkLeave}
+        >
+          GitHub
+        </a>
+        <a
+          href="https://www.linkedin.com/in/mark-ohanesian/"
+          style={linkStyle}
+          onMouseEnter={handleLinkHover}
+          onMouseLeave={handleLinkLeave}
+        >
+          LinkedIn
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
