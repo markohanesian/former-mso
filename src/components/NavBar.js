fb79b1f5c1a3fc714741b6fd040db310fb5033a7
mso872 <mso872@gmail.com>
2024-07-05 12:09:03 -0700

    Refactor NavBar to use Theme

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,35 +1,39 @@
+import React from "react";
 import Logo from "./Logo";
 import ExportButton from "./ExportButton";
-
-const navbarStyles = {
-  margin: 0,
-  width: "auto",
-  height: "72px",
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-  backgroundColor: "#333",
-  padding: "21px 20px",
-};
-
-const sectionStyles = {
-  display: "flex",
-  alignItems: "center",
-};
-
-const PageTitleStyle = {
-  color: "#fff",
-  fontFamily: "sans-serif",
-  fontWeight: "400",
-};
+import { useTheme } from "../theme/ThemeContext";
 
 export default function Navbar() {
+  const { colors } = useTheme();
+
+  const navbarStyles = {
+    margin: 0,
+    width: "auto",
+    height: "72px",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: colors.primary, // Use primary color from theme
+    padding: "21px 20px",
+  };
+
+  const sectionStyles = {
+    display: "flex",
+    alignItems: "center",
+  };
+
+  const PageTitleStyle = {
+    color: colors.textPrimary, // Use primary text color from theme
+    fontFamily: "sans-serif",
+    fontWeight: 400,
+  };
+
   return (
     <header style={navbarStyles}>
       <div style={{ width: "88px" }}>
         <Logo />
       </div>
-      <div style={{ ...sectionStyles }}>
+      <div style={sectionStyles}>
         <h1 style={PageTitleStyle}>FORMER</h1>
       </div>
       <div style={sectionStyles}>
