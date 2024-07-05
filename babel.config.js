341b4f2cfa7b87a16eb467a725a5eef85ca1f687
mso872 <mso872@gmail.com>
2024-07-05 16:20:14 -0700

    Create a babel config file

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    presets: [
+      '@babel/preset-env', // Transpile modern JavaScript to a compatible version
+      '@babel/preset-react', // Add support for JSX syntax
+    ],
+  };
+  
