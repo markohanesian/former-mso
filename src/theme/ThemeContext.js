007911868776ffaff9ff4f28ece48b38e4ed1c62
mso872 <mso872@gmail.com>
2024-07-05 11:44:08 -0700

    Update theme

--- src/theme/ThemeContext.js
+++ src/theme/ThemeContext.js
@@ -5,16 +5,17 @@ const ThemeContext = createContext(null);
 export const ThemeProvider = ({ children, customTheme }) => {
   const defaultTheme = {
     colors: {
-      primary: "#333", // main app theme color
+      primary: "#333", // main app theme dark color
       primaryLight: "rgba(0, 0, 0, 0.1)",
-      secondary: "#FF5733", // app secondary color
+      secondary: "#fff", // main app theme light color
+      tertiary: "#FF5733", // app secondary color
       error: "#D32F2F", // for elements that the user should be made aware of
       warning: "#ED6C02", // for potentially dangerous actions or important messages
       info: "#0288D1", // for highlighting neutral information
       success: "#2E7D32", // for indicating the successful completion of an action that the user triggered
       // component related colors
-      buttonPrimary: "#333",
-      buttonSecondary: "#fff",
+      buttonPrimary: "#333", // dark main color
+      buttonSecondary: "#fff", //light main color
       link: "#0969da",
       // text colors
       textPrimary: "#fff",
