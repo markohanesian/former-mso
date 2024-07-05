58391cf69d840b90aa6d99ee9b1fcc688b803976
mso872 <mso872@gmail.com>
2024-07-05 10:00:00 -0700

    Prettier

--- src/theme/ThemeContext.js
+++ src/theme/ThemeContext.js
@@ -1,30 +1,28 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext } from "react";
 
 const ThemeContext = createContext(null);
 
 export const ThemeProvider = ({ children, customTheme }) => {
   const defaultTheme = {
     colors: {
-      primary: '#333', // main app theme color
-      secondary: '#FF5733', // app secondary color
-      error: '#D32F2F', // for elements that the user should be made aware of
-      warning: '#ED6C02', // for potentially dangerous actions or important messages
-      info: '#0288D1', // for highlighting neutral information
-      success: '#2E7D32', // for indicating the successful completion of an action that the user triggered
+      primary: "#333", // main app theme color
+      secondary: "#FF5733", // app secondary color
+      error: "#D32F2F", // for elements that the user should be made aware of
+      warning: "#ED6C02", // for potentially dangerous actions or important messages
+      info: "#0288D1", // for highlighting neutral information
+      success: "#2E7D32", // for indicating the successful completion of an action that the user triggered
       // component related colors
       link: "#0969da",
       // text colors
       textPrimary: "#fff",
-      textSecondary: '#333'
+      textSecondary: "#333",
     },
   };
 
   const theme = { ...defaultTheme, ...customTheme };
 
   return (
-    <ThemeContext.Provider value={theme}>
-      {children}
-    </ThemeContext.Provider>
+    <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>
   );
 };
 
