9ff426008f972381af60290b0d2d0601135ea9d3
mso872 <mso872@gmail.com>
2024-07-05 12:04:57 -0700

    Remove old logic

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,38 +19,22 @@ export default function Footer() {
     borderTop: `1px solid ${colors.primaryLight}`, 
   };
 
-  const handleLinkHover = (event) => {
-    event.target.style.color = "#666"; // Change color on hover
-    event.target.style.borderBottomColor = "#666"; // Show underline on hover
-  };
-
-  const handleLinkLeave = (event) => {
-    event.target.style.color = "#0969da"; // Restore default color
-    event.target.style.borderBottomColor = "transparent"; // Hide underline
-  };
-
   return (
     <footer style={footerStyle}>
       <div>
         <PersonalCopyright />
         <Link
           href="mailto:mso872@gmail.com"
-          onMouseEnter={handleLinkHover}
-          onMouseLeave={handleLinkLeave}
         >
           Email
         </Link>
         <Link
           href="https://github.com/markohanesian"
-          onMouseEnter={handleLinkHover}
-          onMouseLeave={handleLinkLeave}
         >
           GitHub
         </Link>
         <Link
           href="https://www.linkedin.com/in/mark-ohanesian/"
-          onMouseEnter={handleLinkHover}
-          onMouseLeave={handleLinkLeave}
         >
           LinkedIn
         </Link>
